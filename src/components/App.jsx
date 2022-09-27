db39f059b9c5e9989fb6fde58b77256fb8f9e4e7
Renat <72465313+teenrage-dev@users.noreply.github.com>
2022-09-27 22:42:04 +0300

    Feat: add start files
     Builder

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import { Notes } from './Notes/Notes';
+
+export const App = () => {
+  return <Notes />;
+};
---
+++ src/components/Notes/Notes.jsx
@@ -0,0 +1,6 @@
+import css from './Notes.module.css';
+import React from 'react';
+
+export const Notes = () => {
+  return <div>Notes</div>;
+};
